c20215977398987679902c9cc09447d40876aeb5
Adam Green <adam.green@adgefficiency.com>
2024-09-03 18:16:21 +1200

    feat

--- pandas-meetup/first.py
+++ pandas-meetup/first.py
@@ -1,1 +1,9 @@
+"""
+https://github.com/ADGEfficiency/data-science-south-projects
+"""
+
 import pandas as pd
+
+raw = pd.read_csv(
+    "https://www.emi.ea.govt.nz/Wholesale/Datasets/Generation/Generation_MD/202406_Generation_MD.csv"
+)
